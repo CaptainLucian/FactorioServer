9dfcc2b0ebe6d4239a1683b5c4911f4e111bb751
Jacob Bulgrin <coffee.lord@gmail.com>
2024-10-29 16:17:16 -0500

    fixing issues post name changes

--- build/StartServer.sh
+++ build/StartServer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sleep 10
-/opt/factorio-mod-loader.sh
+sleep 5
+/opt/mod-update-internal.sh
+sleep 1
 /opt/factorio/bin/x64/factorio --server-settings /opt/factorio/data/server-settings.json --start-server-load-latest --console-log /opt/Factorio.log
